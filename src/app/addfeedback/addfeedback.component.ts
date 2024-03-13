967571f82a61c100ad6f774bd46459209bfe8479
Satish <135197530+Mallidi1@users.noreply.github.com>
2024-03-13 11:49:43 +0530

    add constructor

--- src/app/addfeedback/addfeedback.component.ts
+++ src/app/addfeedback/addfeedback.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-addfeedback',
-  templateUrl: './addfeedback.component.html',
-  styleUrls: ['./addfeedback.component.css']
+  selector: "app-addfeedback",
+  templateUrl: "./addfeedback.component.html",
+  styleUrls: ["./addfeedback.component.css"],
 })
 export class AddfeedbackComponent {
-
+  constructor() {}
+  mgOnInit(): void {}
 }
